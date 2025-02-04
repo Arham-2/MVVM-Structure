a05e54b02211f94ae346fc26d5d3484e5682f345
Arham Qayyum <arham@msd-my.com>
2025-02-04 18:48:13 +0500

    Comments added and change of variable name and remove extra code

--- MVVM Structure/PeopleViewModel.swift
+++ MVVM Structure/PeopleViewModel.swift
@@ -24,7 +24,7 @@ class PeopleViewModel {
         apiService.fetchPeople { [weak self] result in
             switch result {
             case .success(let people):
-                self?.allPersons = people
+                self?.allPersons = people              // Assigning People Data to allPersons Array after success in                                              fetching data from API
                 self?.filteredPersons = people
                 self?.didFetchData?()
             case .failure(let error):
@@ -32,9 +32,9 @@ class PeopleViewModel {
             }
         }
     }
-    func filterForSearchTextAndScopeButton(searchText: String, isSearching: Bool) {
+    func filterForSearchText(searchText: String, isSearching: Bool) {
         if isSearching && !searchText.isEmpty {
-            filteredPersons = allPersons.filter() {
+            filteredPersons = allPersons.filter() {                         // Function to search the Person based on                                                                   name, username and email. Also compare from                                                                 the searchText
                  $0.name.lowercased().contains(searchText.lowercased()) ||
                 $0.username.lowercased().contains(searchText.lowercased()) ||
                 $0.email.lowercased().contains(searchText.lowercased())
